2366d36c1f23e391823fc3adfd280a7ca87d211e
Mauricio <mauriciobraga100@gmail.com>
2025-02-25 00:23:02 -0300

    testando og dinamico noticias

--- src/app/noticias/[slug]/page.tsx
+++ src/app/noticias/[slug]/page.tsx
@@ -2,19 +2,37 @@ import { getNewsPageAction } from "@/app/noticias/actions";
 import { LayoutGeneral } from "@/components/layouts/layout-general";
 import { LayoutInterno } from "@/components/layouts/layout-interno";
 import { NewsContent } from "@/components/news/news-content";
-import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 export const revalidate = 20;
+const newsPageData = await getNewsPageAction();
 
-export const metadata: Metadata = {
-  title: "Categoriza Brasil - Notícia",
-  description: "Conheça as últimas notícias do projeto Categoriza Brasil",
-};
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const newsData = newsPageData.find((news) => news.slug === params.slug);
+  if (!newsData) {
+    notFound();
+  }
+
+  return {
+    title: `Categoriza Brasil - ${newsData.titulo}`,
+    description: newsData.texto,
+    openGraph: {
+      title: `Categoriza Brasil - ${newsData.titulo}`,
+      description: newsData.texto,
+      images: [
+        {
+          url: newsData.imagemPrincipal,
+          width: 800,
+          height: 600,
+          alt: newsData.titulo,
+        },
+      ],
+    },
+  };
+}
 
 export default async function Noticia({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  const newsPageData = await getNewsPageAction();
   const newsData = newsPageData.find((news) => news.slug === slug);
 
   if (!newsData) {
