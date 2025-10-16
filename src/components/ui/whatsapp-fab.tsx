23c273f43c875f9d758ad8753fc24598b860ab6d
Mauricio <mauriciobraga100@gmail.com>
2025-10-15 21:45:24 -0300

    att estilos

--- src/components/ui/whatsapp-fab.tsx
+++ src/components/ui/whatsapp-fab.tsx
@@ -37,7 +37,7 @@ export const WhatsappFab = () => {
   );
 
   return (
-    <div className="fixed bottom-4 right-4 md:bottom-6 md:right-6 z-40">
+    <div className="fixed bottom-6 right-4 md:bottom-6 md:right-6 z-40">
       {isMobile ? (
         whatsappButton
       ) : (
